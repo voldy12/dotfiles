4c2b1b0cecf104ae1c42963643f12412fff2967a
Ram <rkm1415@gmail.com>
2021-08-18 09:21:15 +0530

    :seedling: added git install settings along with delta
    nil

---
+++ .install/gitconfig.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git config --global user.name Ram
+git config --global user.email rkm1415@gmail.com
+git config --global core.pager "delta --line-numbers --dark"
+git config --global delta.side-by-side true
