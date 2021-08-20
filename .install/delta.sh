58c16f826f25ac3635d7af3f423d50d57752cdc7
Ram <rkm1415@gmail.com>
2021-08-20 09:13:13 +0530

    ✨ added dust installer

---
+++ .install/delta.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -e
+bash ../binary-scripts/bin/install_from_github  dandavison/delta amd64.deb musl 
---
+++ .install/dust.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -e
+bash ../binary-scripts/bin/install_from_github  bootandy/dust x86_64-unknown-linux-gnu.tar.gz musl 1&> filename.txt
