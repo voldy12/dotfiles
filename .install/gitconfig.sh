328d0fc9379efb0cdff62cb76b27568dd8179f28
Ram <rkm1415@gmail.com>
2021-09-26 01:20:11 +0530

    changed default branch name to main in git

--- .install/gitconfig.sh
+++ .install/gitconfig.sh
@@ -2,5 +2,7 @@
 
 git config --global user.name Ram
 git config --global user.email rkm1415@gmail.com
+git config --global init.defaultBranch main
+# Use git-delta for viewing diff globally
 git config --global core.pager "delta --line-numbers --dark"
 git config --global delta.side-by-side true
